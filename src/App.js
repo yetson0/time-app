9b6c489fe836167277e0092bd2c905585d37bfb2
yetson <yetson@gmail.com>
2023-09-28 23:22:44 +0200

    main App.js init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import GetTimezones from './pages/GetTimezones';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Timezone-app</h1>
+      <Home vars='dummyValue'/>
+      {/* <GetTimezones /> */}
     </div>
   );
 }
